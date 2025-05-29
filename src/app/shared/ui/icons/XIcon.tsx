ba391cd7b7956b22fb3e100c5cfe616d0137964a
leonelvalentinn <leonelvalentinn@gmail.com>
2025-05-29 12:50:29 -0600

    Add CameraIcon and XIcon components, and update index.ts to export them

---
+++ src/app/shared/ui/icons/XIcon.tsx
@@ -0,0 +1,19 @@
+export const XIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth={2}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={`icon icon-tabler icons-tabler-outline icon-tabler-x ${className || ''}`}
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M18 6l-12 12' />
+      <path d='M6 6l12 12' />
+    </svg>
+  )
+}
